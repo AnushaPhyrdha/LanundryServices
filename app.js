b621e03e5cb177deb5ca62edaaaa882ba466216e
manchala1 <88335054+manchala1@users.noreply.github.com>
2021-11-01 21:03:21 +0530

    added minor fixes

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-const db = "mongodb+srv://Ashutosh:LaundryPassword@cluster0.qd20p.mongodb.net/LaundryServices?retryWrites=true&w=majority"
+const db =
+  "mongodb+srv://Ashutosh:LaundryPassword@cluster0.qd20p.mongodb.net/LaundryServices?retryWrites=true&w=majority";
 
-mongoose.connect(db, {
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("MongoDB Connection Successful")
-}).catch((err) => {
-    console.log('MongoDB Connection Error', err)
-})
+  })
+  .then(() => {
+    console.log("MongoDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error", err);
+  });
+app.use(cors());
 
-require('./models/user');
-
+require("./models/user");
 
 app.use(express.json());
-app.use(require('./routes/auth'));
-
-
-
+app.use(require("./routes/auth"));
 
 app.listen(port, () => {
-    console.log(`Server is running on Port :: ${port}`)
-})
-
-
+  console.log(`Server is running on Port :: ${port}`);
+});
--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,24 +1,24 @@
 const jwt = require("jsonwebtoken");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const JWT_Secret = "iamnotbrillientbutiamuniqueiamasoftwareengineer";
 
 const User = mongoose.model("User");
 
 module.exports = async (req, res, next) => {
-    const { authorization } = req.headers;
-    if (!authorization) {
-        return res.status(401).json({ error: "You must be logged in. :: 1" })
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "You must be logged in. :: 1" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  await jwt.verify(token, JWT_Secret, (err, payload) => {
+    if (err) {
+      return res.status(401).json({ error: "You must be logged in. :: 2" });
     }
-    const token = authorization.replace("Bearer ", "");
-    await jwt.verify(token, JWT_Secret, (err, payload) => {
-        if (err) {
-            return res.status(401).json({ error: "You must be logged in. :: 2" })
-        }
-        const { _id } = payload
-        User.find({ _id }).then(userdata => {
-            req.user = userdata
-            next()
-        })
-    })
-}
+    const { _id } = payload;
+    User.find({ _id }).then((userdata) => {
+      req.user = userdata;
+      next();
+    });
+  });
+};
