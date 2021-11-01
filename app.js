babccaabd30603223be3625037e81bc5f711b995
manchala1 <88335054+manchala1@users.noreply.github.com>
2021-11-01 21:07:12 +0530

    merge conflicts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const OrderRoutes = require("./routes/order");
@@ -88,6 +89,13 @@ app.use(express.json());
 app.use(require("./routes/auth"));
 app.use("/orders", OrderRoutes);
 app.use("/get", GetDetailsRoutes);
+
+app.use(cors());
+
+require("./models/user");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`Server is running on Port :: ${port}`);
