f4cc662739f745324909eaefbbf0467ec964010e
unknown <anusha.phyrdha@gmail.com>
2021-11-01 20:18:33 +0530

    After Adding Order API

--- app.js
+++ app.js
@@ -1,31 +1,94 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
+const OrderRoutes = require("./routes/order");
+const GetDetailsRoutes = require("./routes/get_userdetails");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
+const db =
+  "mongodb+srv://Ashutosh:LaundryPassword@cluster0.qd20p.mongodb.net/LaundryServices?retryWrites=true&w=majority";
 
-const db = "mongodb+srv://Ashutosh:LaundryPassword@cluster0.qd20p.mongodb.net/LaundryServices?retryWrites=true&w=majority"
-
-mongoose.connect(db, {
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("MongoDB Connection Successful")
-}).catch((err) => {
-    console.log('MongoDB Connection Error', err)
-})
+  })
+  .then(() => {
+    console.log("MongoDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error", err);
+  });
 
-require('./models/user');
+app.use(cors());
+require("./models/user");
+require("./models/order");
 
+// app.use("/orders", function (req, res, next) {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+//     console.log(token);
+//     if (!token) {
+//       return res.status(401).json({
+//         status: "failed",
+//         message: "Not Authenticated",
+//       });
+//     }
+//     const decoded = jwt.verify(token, "");
+
+//     if (!decoded) {
+//       return res.status(401).json({
+//         status: "failed",
+//         message: "Invalid Token",
+//       });
+//     }
+//     req.user = decoded.data;
+//   } catch (e) {
+//     return res.status(500).json({
+//       status: "failed",
+//       message: e.message,
+//     });
+//   }
+
+//   next();
+// });
+
+// app.use("/get", function (req, res, next) {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+//     console.log(token);
+//     if (!token) {
+//       return res.status(401).json({
+//         status: "failed",
+//         message: "Not Authenticated",
+//       });
+//     }
+//     const decoded = jwt.verify(token, "Insta-Secret-123");
+
+//     if (!decoded) {
+//       return res.status(401).json({
+//         status: "failed",
+//         message: "Invalid Token",
+//       });
+//     }
+//     req.user = decoded.data;
+//   } catch (e) {
+//     return res.status(500).json({
+//       status: "failed",
+//       message: e.message,
+//     });
+//   }
+
+//   next();
+// });
 
 app.use(express.json());
-app.use(require('./routes/auth'));
-
-
-
+app.use(require("./routes/auth"));
+app.use("/orders", OrderRoutes);
+app.use("/get", GetDetailsRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on Port :: ${port}`)
-})
-
-
+  console.log(`Server is running on Port :: ${port}`);
+});
